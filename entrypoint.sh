e3dbf533b64f227d022d9d9d764098fbf1359591
Zhu, Peihong <peihong.zhu@ge.com>
2017-11-09 08:05:52 -0500

    updating testDataServer.py (#4)

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,12 @@ if test ${DOWNLOAD_RAD_DEMO_DATA:-off} = 'on' || test ${DOWNLOAD_RAD_DEMO_DATA:-
   (./download_rad_demo_data.sh && python create_rad_demo_datastore.py) &
 fi
 
-python /rt106/dataServer.py
+if test ${TEST:--false} = 'coverage'; then
+  python /rt106/dataServer.py &
+  py.test --cov-report term --cov=. testDataServer.py
+  #py.test --cov-report html --cov=. testDataServer.py
+  #mv htmlcov /data
+else
+  python /rt106/dataServer.py
+fi
+
--- testDataServer.py
+++ testDataServer.py
@@ -9,14 +9,14 @@ idx = sys.argv.index('testDataServer.py')
 sys.argv = sys.argv[idx:]
 print(sys.argv)
 from dataServer import app
+import shutil
 
-patientList = ['AGA_260_3','pat001','pat002','pat003','pat004','pat005','pat006']
-studyList = ['studyA']
-seriesList = ['cardiac_Bias_600_Cardiac_3T_series6_PURE']
-imageName = 'i851332.MRDC.1'
-slideList = ['AGA_260_3']
-regionList = ['021']
-channelList = ['DAPI']
+PATIENT = "Adam"
+STUDY = "1.2.826.0.1.3680043.2.1125.1.75064541463040.2005072610175534421"
+SERIES = "017f45ac-ce32-4921-8700-89d6b0bedb21"
+SERIES_PATH = "/Patients/Adam/Primary/Imaging/"+ STUDY + '/' + SERIES
+INSTANCE_PATH = "/Patients/Adam/Primary/Imaging/"+ STUDY + '/' + SERIES + '/' + 'vhm.1001.dcm'
+UPLOAD_PATH = '/Patients/'+ PATIENT + '/Results/test_pipeline/test_execid/Imaging/test_study'
 
 class TestDataServerAPIs(unittest.TestCase):
 
@@ -31,117 +31,239 @@ class TestDataServerAPIs(unittest.TestCase):
         url = 'http://localhost:5106/invalidinput'
         response = self.app.get(url,content_type='application/json')
         self.assertEqual(response.status_code, 404)
-
-    def test_bad_request(self):
-        url = 'http://localhost:5106/%'
-        response = self.app.get(url,content_type='application/json')
-        self.assertEqual(response.status_code, 400)
-
+    
     def test_health_check(self):
         url = 'http://localhost:5106/v1/health'
         response = self.app.get(url,content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
     def test_health(self):
-        url = 'http://localhost:5106/v1'
+        url = 'http://localhost:5106/'
         response = self.app.get(url,content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
     def test_get_patient_list(self):
-        url = 'http://localhost:5106/api/v1/patients'
+        url = 'http://localhost:5106/v1/patients'
         response = self.app.get(url,content_type='application/json')
         self.assertEqual(response.status_code, 200)
         json_res = json.loads(response.data)
-        #self.assertTrue(any(json_res))
-        patients = [p['patientName'] for p in json_res]
-        self.assertEqual(patients, patientList)
+        patients = [p['id'] for p in json_res]
+        self.assertTrue(len(patients) > 0)
+        
+    def test_get_patient_info(self):
+        url = 'http://localhost:5106/v1/patients/' + PATIENT
+        response = self.app.get(url,content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        json_res = json.loads(response.data)
+        primary_type = json_res['primary']
+        self.assertEqual(primary_type[0], 'Imaging')
 
     def test_get_study_list(self):
-        url = 'http://localhost:5106/api/v1/pat001/study'
+        url = 'http://localhost:5106/v1/patients/' + PATIENT + '/imaging/studies'
         response = self.app.get(url,content_type='application/json')
         self.assertEqual(response.status_code, 200)
         json_res = json.loads(response.data)
-        studies = [s['id'] for s in json_res]
-        self.assertEqual(studies, studyList)
-
+        self.assertEqual(json_res[0]['eid'], 'primary')
+        
+    def test_get_study_type(self):
+        url = 'http://localhost:5106/v1/patients/' + PATIENT + '/imaging/studies/' + STUDY + '/types'
+        response = self.app.get(url,content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        json_res = json.loads(response.data)
+        self.assertEqual(json_res[0], 'series')
+        
+    def test_get_primary_series_list(self):
+        url = 'http://localhost:5106/v1/patients/' + PATIENT + '/imaging/studies/' + STUDY + '/primary'
+        response = self.app.get(url,content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        json_res = json.loads(response.data)
+        self.assertEqual(json_res[0]['eid'], 'primary')
+        self.assertTrue(len(json_res[0]['id'])>0)
+        
     def test_get_series_list(self):
-        url = 'http://localhost:5106/api/v1/pat001/studyA/series'
+        url = 'http://localhost:5106/v1/patients/' + PATIENT + '/imaging/studies/' + STUDY + '/series'
         response = self.app.get(url,content_type='application/json')
         self.assertEqual(response.status_code, 200)
         json_res = json.loads(response.data)
-        self.assertEqual(json_res[0]['id'], seriesList[0])
-        #self.assertTrue(any(json_res))
+        self.assertEqual(json_res[0]['eid'], 'primary')
+        self.assertTrue(len(json_res[0]['id'])>0)     
+    
+    def test_get_series_type(self):
+        url = 'http://localhost:5106/v1/series' + SERIES_PATH +'/types'
+        response = self.app.get(url,content_type='application/json')
+        self.assertEqual(response.status_code, 200)
 
+    def test_get_image_list(self):
+        url = 'http://localhost:5106/v1/series' + SERIES_PATH +'/instances'
+        response = self.app.get(url,content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        json_res = json.loads(response.data)
+        self.assertTrue(len(json_res['paths'])>0)   
+        
+    def test_get_uploading_path(self):
+        url =  'http://localhost:5106/v1/patients/' + PATIENT + '/results/test_pipeline/steps/test_execid/imaging/studies/test_study/series'
+        response = self.app.get(url,content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        json_res = json.loads(response.data)
+        self.assertEqual(json_res['path'],UPLOAD_PATH)
+     
     def test_retrieve_series(self):
-        url = 'http://localhost:5106/v1/series/pat001/'+ str(studyList[0]) + '/'+ str(seriesList[0])
+        url = 'http://localhost:5106/v1/series'+ SERIES_PATH + '/archive'
         response = self.app.get(url,content_type='application/json')
         self.assertEqual(response.status_code, 200)
         self.assertTrue(any(response.data))
-
+        
     def test_upload_series(self):
-        tar = tarfile.open('/tmp/output.tar','w')
-        for f in glob.glob('/test/upload/radiology'):
+        tar = tarfile.open('./output.tar','w')
+        for f in glob.glob('/rt106/data' + SERIES_PATH + '/'):
             filename = os.path.basename(f)
             tar.add(f,arcname=filename)
         tar.close()
-        archive = { 'file' : open('/tmp/output.tar' ,'rb') }
-        #self.assertEqual(archive, 200)
-        url = 'http://localhost:5106/v1/series/pat001/'+ str(studyList[0]) + '/'+ str(seriesList[0])+'_'+ str(uuid.uuid4())+ '/Test_Derived.tar'
+        archive = { 'file' : open('./output.tar' ,'rb') }
+        upload_path = '/uploads/Radiology/test'
+        url = 'http://localhost:5106/v1/series' + upload_path + '/tar'
         response = self.app.post(url,data=archive)
         self.assertEqual(response.status_code, 200)
-
-    def test_get_series_instance(self):
-        url = 'http://localhost:5106/v1/instance/pat001/'+ str(studyList[0]) + '/'+ str(seriesList[0]) + '/' + imageName
-        response = self.app.get(url,content_type='application/json')
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue(any(response.data))
-
-    def test_get_series_metadata(self):
-        url = 'http://localhost:5106/v1/series/list/pat001/'+ str(studyList[0]) + '/'+ str(seriesList[0])
+        os.remove('./output.tar')
+        shutil.rmtree('/rt106/data' + upload_path)
+        shutil.rmtree('/rt106/data/uploads/')
+    
+    def test_get_instance_type(self):
+        url = 'http://localhost:5106/v1/instance' + INSTANCE_PATH +'/type'
         response = self.app.get(url,content_type='application/json')
         self.assertEqual(response.status_code, 200)
         json_res = json.loads(response.data)
-        self.assertTrue(any(json_res))
+        self.assertEqual(json_res['type'],'DICOM')
+    
+    def test_get_instance(self):
+        url = 'http://localhost:5106/v1/instance' + INSTANCE_PATH +'/DICOM'
+        response = self.app.get(url,content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(any(response.data))        
+    
+    def test_upload_instance(self):
+        url = 'http://localhost:5106/v1/instance/uploads/Pathology/test1/tiff16'
+        filedir = '/rt106/data/Slides/AGA_260_3/021/Source/DAPI/pERK_CD31_AGA_260_3_S001_P021_dapi.tif'
+        shutil.copyfile(filedir, './DAPI.tif')
+        archive = { 'file' : open('./DAPI.tif' ,'rb') }    
+        response = self.app.post(url,data=archive)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(os.path.isfile('/rt106/data/uploads/Pathology/test1/DAPI.tif'))
+        shutil.rmtree('/rt106/data/uploads/Pathology/test1/')
+        shutil.rmtree('/rt106/data/uploads/')
+        
+    def test_upload_instance_force(self):
+        url = 'http://localhost:5106/v1/instance/uploads/Pathology/test2/tiff16/force'
+        filedir = '/rt106/data/Slides/AGA_260_3/021/Source/DAPI/pERK_CD31_AGA_260_3_S001_P021_dapi.tif'
+        shutil.copyfile(filedir, './DAPI.tif')
+        archive = { 'file' : open('./DAPI.tif' ,'rb') }    
+        response = self.app.post(url,data=archive)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(os.path.isfile('/rt106/data/uploads/Pathology/test2/DAPI.tif'))
+        shutil.rmtree('/rt106/data/uploads/Pathology/test2/')
+        shutil.rmtree('/rt106/data/uploads/')
 
+    def test_get_annotation_type(self):
+        url = 'http://localhost:5106/v1/annotation' + SERIES_PATH + '/types'
+        response = self.app.get(url,content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        
     def test_get_annotation(self):
-        url = 'http://localhost:5106/v1/annotation/pat001/'+ str(studyList[0]) + '/'+ str(seriesList[0])
+        url = 'http://localhost:5106/v1/annotation' + SERIES_PATH + '/JSON'
         response = self.app.get(url,content_type='application/json')
         self.assertEqual(response.status_code, 200)
         json_res = json.loads(response.data)
         self.assertTrue(any(json_res))
-
+    
+    #
+    # Pathology / microscopy section 
+    #
+        
     def test_get_slide_list(self):
-        url = 'http://localhost:5106/v1/pathology/slide'
+        url = 'http://localhost:5106/v1/pathology/slides'
         response = self.app.get(url,content_type='application/json')
         self.assertEqual(response.status_code, 200)
         json_res = json.loads(response.data)
-        self.assertIn(slideList[0], json_res)
+        self.assertIn("AGA_260_3", json_res)
+        
+    def test_get_slide_type(self):
+        url = 'http://localhost:5106/v1/pathology/slides/AGA_260_3/types'
+        response = self.app.get(url,content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        json_res = json.loads(response.data)
+        self.assertIn('regions', json_res)
 
     def test_get_slide_regions(self):
-        url = 'http://localhost:5106/v1/pathology/' + str(slideList[0]) + '/region'
+        url = 'http://localhost:5106/v1/pathology/slides/AGA_260_3/regions'
         response = self.app.get(url,content_type='application/json')
         self.assertEqual(response.status_code, 200)
         json_res = json.loads(response.data)
-        self.assertTrue(any(json_res))
+        self.assertIn('021', json_res)
+        
+    def test_get_region_type(self):
+        url = 'http://localhost:5106/v1/pathology/slides/AGA_260_3/regions/021/types'
+        response = self.app.get(url,content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        json_res = json.loads(response.data)
+        self.assertIn('channels', json_res)
 
     def test_get_slide_channels(self):
-        url = 'http://localhost:5106/v1/pathology/' + str(slideList[0]) + '/' + str(regionList[0]) +'/channel'
+        url = 'http://localhost:5106/v1/pathology/slides/AGA_260_3/regions/021/channels'
         response = self.app.get(url,content_type='application/json')
         self.assertEqual(response.status_code, 200)
         json_res = json.loads(response.data)
-        self.assertTrue(any(json_res))
+        self.assertIn('DAPI', json_res)
 
-    def test_get_slide_image(self):
-        url = 'http://localhost:5106/v1/pathology/image/' + str(slideList[0]) + '/' + str(regionList[0])+'/source/'+ str(channelList[0])
+    def test_get_channel_type(self):
+        url = 'http://localhost:5106/v1/pathology/slides/AGA_260_3/regions/021/channels/DAPI/types'
         response = self.app.get(url,content_type='application/json')
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(any(response.data))
+        json_res = json.loads(response.data)
+        self.assertEqual('tiff16', json_res[0])
+        
+    def test_get_image_path(self):
+        url = 'http://localhost:5106/v1/pathology/slides/AGA_260_3/regions/021/channels/DAPI/image'
+        response = self.app.get(url,content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        json_res = json.loads(response.data)
+        self.assertTrue(len(json_res[0]) > 0)
+        
+    def test_get_result_types(self):
+        url = 'http://localhost:5106/v1/pathology/slides/AGA_260_3/regions/021/results/test_pipeline/types'
+        response = self.app.get(url,content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        json_res = json.loads(response.data)
+        self.assertEqual('steps', json_res[0])
 
-    # def test_upload_pathology_result(self):
-    #     archive = { 'file' : open('/test/upload/pathology/DAPI.tif' ,'rb') }
-    #     url = 'http://localhost:5106/v1/pathology/result/'+ str(slideList[0]) + '/' + str(regionList[0])+'/derived/'+ str(channelList[0])
-    #     response = self.app.post(url,data=archive)
-    #     self.assertEqual(response.status_code, 200)
+    def test_get_result_format(self):
+        url = 'http://localhost:5106/v1/pathology/slides/AGA_260_3/regions/021/results/Source/steps/DAPI'
+        # need to have the result in the directory to test on
+        response = self.app.get(url,content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        json_res = json.loads(response.data)
+        self.assertIn('tiff16', json_res)
+                
+    def test_get_result_path(self):
+        url = 'http://localhost:5106/v1/pathology/slides/AGA_260_3/regions/021/results/Source/steps/DAPI/data'
+        response = self.app.get(url,content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        json_res = json.loads(response.data)
+        self.assertTrue(len(json_res) > 0)
+        
+    def test_get_result_image_path(self):
+        url = 'http://localhost:5106/v1/pathology/slides/AGA_260_3/regions/021/results/Source/steps/DAPI/instances'
+        response = self.app.get(url,content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        json_res = json.loads(response.data)
+        self.assertTrue(len(json_res) > 0)
+        
+    def test_get_pipeline_list(self):
+        url = 'http://localhost:5106/v1/pathology/slides/AGA_260_3/regions/021/results'
+        response = self.app.get(url,content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+    
+    # need to have at least one pipeline available for results    
+    #def test_get_execution_list(self):
 
 if __name__ == '__main__':
     unittest.main()
